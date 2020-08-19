5f6801440ddeb7fb402d355791c4325d2916f7e7
mayank930 <69713233+mayank930@users.noreply.github.com>
2020-08-19 23:54:46 +0530

    Update Assignment_Day3.java
    Changes Made: Stringname.length()

--- Day3/Assignment_Day3.java
+++ Day3/Assignment_Day3.java
@@ -3,20 +3,22 @@ import java.util.Scanner;
 public class Assignment_Day3 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter First Word of 6 letters:");
+		System.out.println("Enter First String:");
 		String String1 = scan.next();
-		System.out.println("Enter Second Word of 6 letters:");
+		System.out.println("Enter Second String:");
 		String String2 = scan.next();
 		
-		String firsttwo = String1.substring(0,2);
-		String rem_String1 = String1.substring(2,6);
+		String firsttwo1 = String1.substring(0,2);
+		String lasttwo1= String1.substring(String1.length()-2,String1.length());
+		String rem_String1 = String1.substring(2,String1.length()-2);
 		
-		String lasttwo = String2.substring(4,6);
-		String rem_String2 = String2.substring(0,4);
+		String firsttwo2 = String2.substring(0,2);
+		String lasttwo2= String2.substring(String2.length()-2,String2.length());
+		String rem_String2 = String2.substring(2,String2.length()-2);
 		
-		String newString1=lasttwo+rem_String1;
+		String newString1=lasttwo2+rem_String1+firsttwo2;
 		System.out.println(newString1);
-		String newString2=rem_String2+firsttwo;
+		String newString2=lasttwo1+rem_String2+firsttwo1;
 		System.out.println(newString2);
 	}	
 }
